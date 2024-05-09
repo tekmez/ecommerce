df64a43bc7a5e01ea593521e58cfd0cb70bd6b12
Tuncay Ekmez <tuncayekmez@gmail.com>
2024-05-10 00:06:43 +0300

    Connect localStorage

--- lib/productSlice.ts
+++ lib/productSlice.ts
@@ -13,7 +13,7 @@ interface Product {
 }
 
 const initialState: ProductState = {
-  basket: [],
+  basket: JSON.parse(localStorage.getItem("basket") || "[]"),
   product: {
     id: "",
     name: "",
@@ -27,25 +27,32 @@ const productSlice = createSlice({
   initialState,
   reducers: {
     setBasket: (state, action: PayloadAction<Product>) => {
-      state.product = action.payload;
-      // if the product is already in the basket, increase the total
-      const index = state.basket.findIndex((p) => p.id === action.payload.id);
-      if (index !== -1) {
-        state.basket[index].total += 1;
-        return;
+      const existingProduct = state.basket.find(
+        (p) => p.id === action.payload.id
+      );
+      if (existingProduct) {
+        existingProduct.total += 1;
+      } else {
+        state.basket.push(action.payload);
       }
-      state.basket = [...state.basket, action.payload];
+      localStorage.setItem("basket", JSON.stringify(state.basket));
     },
     increaseTotal: (state, action: PayloadAction<string>) => {
-      const index = state.basket.findIndex((p) => p.id === action.payload);
-      state.basket[index].total += 1;
+      const product = state.basket.find((p) => p.id === action.payload);
+      if (product) {
+        product.total += 1;
+      }
+      localStorage.setItem("basket", JSON.stringify(state.basket));
     },
     decreaseTotal: (state, action: PayloadAction<string>) => {
       const index = state.basket.findIndex((p) => p.id === action.payload);
-      state.basket[index].total -= 1;
-      if (state.basket[index].total === 0) {
-        state.basket = state.basket.filter((p) => p.id !== action.payload);
+      if (index !== -1) {
+        state.basket[index].total -= 1;
+        if (state.basket[index].total === 0) {
+          state.basket.splice(index, 1);
+        }
       }
+      localStorage.setItem("basket", JSON.stringify(state.basket));
     },
   },
 });
